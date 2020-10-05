8ea7f6226a9ba66179feba7522e0033480eef3ca
n1ckDotEXE <admin@nicholasnam.com>
2020-10-05 15:55:09 -0400

    Work in progress

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
-function challenge1(str) {
-    let i = 0;
-    while (i < str.length) {
-        console.log(str[i]);
-        i = i + 1;
+// function challenge1(str) {
+//     let i = 0;
+//     while (i < str.length) {
+//         console.log(str[i]);
+//         i = i + 1;
+//     }
+// }
+
+// challenge1('Hello.');
+// challenge1('Oh hi.');
+
+// function challenge2(str) {
+//     let i = 0;
+//     while (i < str.length) {
+//         console.log (str[i]);
+//         i = i + 3;
+//     }
+// }
+
+// challenge2 ('I am the alfalfa and the omelette');
+
+// function challenge3 (str) {
+//     let i = str.length -1;
+//     while (i >= 0) {
+//         console.log (str[i]);
+//         i = i - 1;
+//     }
+// } 
+
+// challenge3 ('Hello');
+// challenge3 ('Oh hi');
+
+
+// INCOMPLETE ///////////////////
+
+function challenge4(str) {
+    let vowels = [];
+    for (i = 0; i < str.length; i++) {
+        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
+            vowels.push(str[i]);
+        }
     }
+    return vowels[0];
 }
 
-challenge1('Hello.');
-challenge1('Oh hi.');
-
-function challenge2(str) {
-    let i = 0;
-    while (i < str.length) {
-        console.log (str[i]);
-        i = i + 3;
-    }
-}
-
-challenge2 ('I am the alfalfa and the omelette');
-
-function challenge3 (str) {
-    let i = str.length -1;
-    while (i >= 0) {
-        console.log (str[i]);
-        i = i - 1;
-    }
-} 
-
-challenge3 ('Hello');
-challenge3 ('Oh hi');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+challenge4('testing');
