5983e1d03764e8af5190b275ae38065b9f2b6e8b
n1ckDotEXE <admin@nicholasnam.com>
2020-10-05 16:13:42 -0400

    Challenge 4 Completed

--- main.js
+++ main.js
@@ -1,46 +1,45 @@
-// function challenge1(str) {
-//     let i = 0;
-//     while (i < str.length) {
-//         console.log(str[i]);
-//         i = i + 1;
-//     }
-// }
+function challenge1(str) {
+    let i = 0;
+    while (i < str.length) {
+        console.log(str[i]);
+        i = i + 1;
+    }
+}
 
-// challenge1('Hello.');
-// challenge1('Oh hi.');
+challenge1('Hello.');
+challenge1('Oh hi.');
 
-// function challenge2(str) {
-//     let i = 0;
-//     while (i < str.length) {
-//         console.log (str[i]);
-//         i = i + 3;
-//     }
-// }
+function challenge2(str) {
+    let i = 0;
+    while (i < str.length) {
+        console.log (str[i]);
+        i = i + 3;
+    }
+}
 
-// challenge2 ('I am the alfalfa and the omelette');
+challenge2 ('I am the alfalfa and the omelette');
 
-// function challenge3 (str) {
-//     let i = str.length -1;
-//     while (i >= 0) {
-//         console.log (str[i]);
-//         i = i - 1;
-//     }
-// } 
+function challenge3 (str) {
+    let i = str.length -1;
+    while (i >= 0) {
+        console.log (str[i]);
+        i = i - 1;
+    }
+} 
 
-// challenge3 ('Hello');
-// challenge3 ('Oh hi');
-
-
-// INCOMPLETE ///////////////////
+challenge3 ('Hello');
+challenge3 ('Oh hi');
 
 function challenge4(str) {
     let vowels = [];
+
+    let lowerCase = str.toLowerCase;
+
     for (i = 0; i < str.length; i++) {
         if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u') {
             vowels.push(str[i]);
+
+            console.log(str[i]);
         }
     }
-    return vowels[0];
 }
-
-challenge4('testing');
