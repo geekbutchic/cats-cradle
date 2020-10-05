5d4bbaf38af3b56edcbe69b6d8875f2e6e92253f
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-10-05 14:59:00 -0400

    started our loops

---
+++ main.js
@@ -0,0 +1,46 @@
+function challenge1(str) {
+    let i = 0;
+    while (i < str.length) {
+        console.log(str[i]);
+        i = i + 1;
+    }
+}
+
+challenge1('Hello.');
+challenge1('Oh hi.');
+
+function challenge2(str) {
+    let i = 0;
+    while (i < str.length) {
+        console.log (str[i]);
+        i = i + 3;
+    }
+}
+
+challenge2 ('I am the alfalfa and the omelette');
+
+function challenge3 (str) {
+    let i = str.length -1;
+    while (i >= 0) {
+        console.log (str[i]);
+        i = i - 1;
+    }
+} 
+
+challenge3 ('Hello');
+challenge3 ('Oh hi');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
